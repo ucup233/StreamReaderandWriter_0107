c7b00343fa1321ad0e371886b8a7cb8458954e05
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-06-03 10:20:25 +0700

    membuka file dalam mode membaca

--- InfilewithPath/InfilewithPath.cpp
+++ InfilewithPath/InfilewithPath.cpp
@@ -12,7 +12,7 @@ int main() {
 	ofstream outfile;
 	outfile.open(NamaFile + ".txt", ios::out);
 
-	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
 	while (true) {
 		cout << "- ";
@@ -21,4 +21,19 @@ int main() {
 		outfile << baris << endl;
 	}
 	outfile.close();
+
+	ifstream infile;
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+
+	if (infile.is_open()) {
+		while (getline(infile, baris)) {
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
+
 }
